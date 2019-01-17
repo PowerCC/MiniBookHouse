d696b55e44f23174236f2e6be857436eef09a942
PowerCC <zoucheng@baihe.com>
2019-01-18 01:34:20 +0800

    分类修改

--- pages/category.js
+++ pages/category.js
@@ -6,120 +6,156 @@ Page({
    * 页面的初始数据
    */
   data: {
-    menu: [],
-    activeIndex: 0,
-    submenu: [],
-  },
-  tapItem: function(e) {
-    let _this = this;
-    this.data.menu.forEach(function (item,index) {
-       if (item.id === e.currentTarget.dataset.id){
-         _this.setData({
-             activeIndex: index,
-         });
-         _this.getData(item.id);
-       }
-    });
-  },
-  openThis: function(e){
-    let params = '?id='+this.data.activeIndex;
-        params += '&item_id='+e.currentTarget.dataset.item_id;
-        params += '&title='+e.currentTarget.dataset.name;
-    wx.navigateTo({
-        url: '/pages/category/list'+params,
-    })
-  },
-  getData: function(pid){
-    let _this = this;
-    wx.request({
-        url: app.globalData.baseApi+"category/getList",
-        method: "GET",
-        data: {
-            pid: pid
-        },
-        success(res){
-            if (res.data.code == 200){
-                if (pid == 0) {
-                    _this.setData({
-                        menu: res.data.data.list,
-                    });
-                    if (res.data.data.list.length > 0) {
-                        _this.getData(_this.data.menu[0].id); //初始加载子列表
-                    }
-                }else{
-                    _this.setData({
-                        submenu: res.data.data.list,
-                    })
-                }
-            }
-        },
-        fail(){
-
-        }
-      });
+    pageBottom: "30rpx",
+    pageIndex: 1,
+    ageItems: [],
+    languageItems: [],
+    categoryItems: [],
+    categoryIds: [],
+    goodsList: [],
+    pageEnd: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-      var _this = this
-      wx.getSystemInfo({
-          success: function(res) {
-              _this.setData({
-                  winHeight: res.windowHeight
-              });
-          }
-      });
-      this.getData(0);
+  onLoad: function(options) {
+    var _this = this;
+
+    let isPhone = app.globalData.isIPhoneX;
+
+    this.setData({
+      pageBottom: isPhone ? "68rpx" : "30rpx"
+    });
+
+    this.getCategoryListByParentId();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
+  },
+
+  getCategoryListByParentId: function() {
+    let _this = this;
+    wx.showNavigationBarLoading();
+    wx.request({
+      url: app.globalData.baseApi + "outer/getCategoryListByParentId",
+      method: "GET",
+      data: {
+        pid: "1,2,3",
+      },
+      success(res) {
+        if (res.data.code == 200) {
+          console.log(res.data);
+
+          let result = res.data.data.result;
+          _this.setData({
+            ageItems: _this.data.ageItems.concat(result.p1),
+            languageItems: _this.data.languageItems.concat(result.p2),
+            categoryItems: _this.data.categoryItems.concat(result.p3)
+          });
+        }
+      },
+      complete() {
+        wx.hideNavigationBarLoading();
+        wx.stopPullDownRefresh();
+      }
+    });
+  },
+
+  getGoodsListByCategoryId: function() {
+    let _this = this;
+    wx.showNavigationBarLoading();
+    wx.request({
+      url: app.globalData.baseApi + "outer/getGoodsListByCategoryId",
+      method: "GET",
+      data: {
+        cid: _this.data.categoryIds,
+      },
+      success(res) {
+        if (res.data.code == 200) {
+          console.log(res.data);
+
+          let result = res.data.data.result;
+          var page = _this.data.pageIndex;
+          var pageEnd = true;
+
+          if (result.length > 0) {
+            page += 1;
+            pageEnd = false;
+          }
+
+          _this.setData({
+            pageIndex: page,
+            goodsList: _this.data.goodsList.concat(result),
+            pageEnd: pageEnd
+          });
+        }
+      },
+      complete() {
+        wx.hideNavigationBarLoading();
+        wx.stopPullDownRefresh();
+      }
+    });
+  },
+
+  categoryTap: function(e) {
+    let cid = e.currentTarget.dataset.id;
+
+    console.log(cid);
+
+    // var ids = this.data.categoryIds;
+    // ids.push(cid.toString());
+
+    // this.setData({
+    //   categoryIds: ids
+    // })
+
+    // this.getGoodsListByCategoryId();
   }
-});
+})
